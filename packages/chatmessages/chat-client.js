e5d49634d0de11c59803743fe181f44da5024ab9
peonmodel <peonmodel+git@gmail.com>
2016-08-01 23:40:12 +0800

    added jsdocs for client-side API
    added jsdocs comments for client-side class for Chat and Message

--- packages/chatmessages/chat-client.js
+++ packages/chatmessages/chat-client.js
@@ -2,34 +2,78 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check, /*Match*/ } from 'meteor/check';
 
+/**
+ * class representing a chat instance,
+ * may create a new chat instance for each room/game/team
+ *
+ */
 class Chat {
+
+  /**
+   * constructor - instantiate Chat instance
+   * see schema
+   *
+   * @param  {object} item object stored in collection
+   */
   constructor(item){
     Object.assign(this, item);
   }
 
+  /**
+   * deleteChat - delete this Chat instance
+   *
+   * @param {function} callback function to call when meteor method returns
+   */
   deleteChat(callback = ()=>{}){
     check(callback, Function);
     Meteor.call(`${Chat.prefix}/deleteChat`, this._id, callback);
   }
 
+  /**
+   * joinChat - add member to Chat instance
+   *
+   * @param {object} member Player/User instance to join the chat
+   * @param {function} callback function to call when meteor method returns
+   */
   joinChat(member, callback = ()=>{}){
     check(member, Object);
     check(callback, Function);
     Meteor.call(`${Chat.prefix}/joinChat`, this._id, member, callback);
   }
 
+  /**
+   * leaveChat - remove member to Chat instance
+   *
+   * @param {object} member Player/User instance to join the chat
+   * @param {function} callback function to call when meteor method returns
+   */
   leaveChat(member, callback = ()=>{}){
     check(member, Object);
     check(callback, Function);
     Meteor.call(`${Chat.prefix}/leaveChat`, this._id, member, callback);
   }
 
+  /**
+   * createMessage - add text to chat as a Message
+   *
+   * @param {string} text content of message
+   * @param {function} callback function to call when meteor method returns
+   */
   createMessage(text, callback = ()=>{}){
     check(text, String);
     check(callback, Function);
     Meteor.call(`${Chat.prefix}/createMessage`, this._id, text, callback);
   }
 
+  /**
+   * createChat - add an instance of Chat to collection
+   * constructor is used to transform the collection, whereas this is to add a Chat to collection
+   *
+   * @param {string} type of chat, e.g. private, group, public, etc
+   * @param {string} title title of chat
+   * @param {array} members users/players in chat
+   * @param {function} callback function to call when meteor method returns
+   */
   static createChat(type = 'private', title = '', members = [], callback = ()=>{}){
     check(type, String);
     check(title, String);
--- packages/chatmessages/message-client.js
+++ packages/chatmessages/message-client.js
@@ -2,30 +2,66 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check, /*Match*/ } from 'meteor/check';
 
-// client-side
+
+/**
+ * class representing a message instance
+ * intended to be used in Chat
+ */
 class Message {
 
+  /**
+   * constructor - instantiate Message instance
+   * see schema
+   *
+   * @param  {object} item object stored in collection
+   */
   constructor(item){
     Object.assign(this, item);
   }
 
+  /**
+   * deleteMessage - delete this Message instance
+   *
+   * @param {function} callback function to call when meteor method returns
+   */
   deleteMessage(callback = ()=>{}){
     check(callback, Function);
     Meteor.call(`${Message.prefix}/deleteMessage`, this._id, callback);
   }
 
+  /**
+   * editMessage - edit text content of this Message instance
+   *
+   * @param {string} text new content of Message
+   * @param {function} callback function to call when meteor method returns
+   */
   editMessage(text, callback = ()=>{}){
     check(text, String);
     check(callback, Function);
     Meteor.call(`${Message.prefix}/editMessage`, this._id, text, callback);
   }
 
+  /**
+   * replyMessage - create a Message instance as reply to another Message instance
+   *
+   * @param {string} text content of reply Message
+   * @param {function} callback function to call when meteor method returns
+   */
   replyMessage(text, callback = ()=>{}){
     check(text, String);
     check(callback, Function);
     Meteor.call(`${Message.prefix}/replyMessage`, this._id, text, callback);
   }
 
+  /**
+   * createMessage - add an instance of Message to collection
+   * constructor is used to transform the collection, whereas this is to add a Message to collection
+   *
+   * @param {string} chatId id of chat Message belongs to
+   * @param {string} text content of Message
+   * @param {Message} replyTo Message object that is being replied to
+   * @param {function} callback function to call when meteor method returns
+   */
   static createMessage(chatId, text, replyTo, callback = ()=>{}){
     check(chatId, String);
     check(text, String);
