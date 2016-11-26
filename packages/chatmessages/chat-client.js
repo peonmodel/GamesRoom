8b783f0eb877b4da97a8001af63ccbd3ff38296f
peonmodel <peonmodel+git@gmail.com>
2016-11-26 12:31:27 +0800

    converted chat and message meteor call prototypes to be async function that returns promise, fixed eslint issues

--- packages/chatmessages/chat-client.js
+++ packages/chatmessages/chat-client.js
@@ -1,6 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
+import { Message } from './message-client.js';
+
+/**
+ * promiseCall - function to wrap async Meteor functions into returning promises
+ *
+ * @param  {Function} fn - async function to wrap
+ * @param  {Array} params - array of params
+ * @returns {Promise}           resolve if success
+ */
+function promiseCall(fn, ...params) {
+	return new Promise((resolve, reject) => {
+		fn(...params, (err, res) => {
+			if (err) { return reject(err); }
+			return resolve(res);
+		});
+	});
+}
 
 /**
  * Client-side.
@@ -28,47 +45,43 @@ class Chat {
 	/**
 	 * deleteChat - delete this Chat instance
 	 *
-	 * @param {function} callback function to call when meteor method returns
+	 * @returns {Promise} - a promise, 1 if successful
 	 */
-	deleteChat(callback = () => {}) {
-	  check(callback, Function);
-	  Meteor.call(`${Chat.prefix}/deleteChat`, this._id, callback);
+	async deleteChat() {
+		return promiseCall(Meteor.call, `${Chat.prefix}/deleteChat`, this._id);
 	}
 
   /**
    * joinChat - add member to Chat instance
    *
    * @param {object} member Player/User instance to join the chat
-   * @param {function} callback function to call when meteor method returns
+   * @returns {Promise} - a promise, 1 if successful
    */
-	joinChat(member, callback = () => {}) {
+	async joinChat(member) {
 	  check(member, Object);
-	  check(callback, Function);
-	  Meteor.call(`${Chat.prefix}/joinChat`, this._id, member, callback);
+		return promiseCall(Meteor.call, `${Chat.prefix}/joinChat`, this._id, member);
 	}
 
   /**
    * leaveChat - remove member to Chat instance
    *
    * @param {object} member Player/User instance to join the chat
-   * @param {function} callback function to call when meteor method returns
+   * @returns {Promise} - a promise, 1 if successful
    */
-	leaveChat(member, callback = () => {}) {
+	async leaveChat(member) {
 	  check(member, Object);
-	  check(callback, Function);
-	  Meteor.call(`${Chat.prefix}/leaveChat`, this._id, member, callback);
+		return promiseCall(Meteor.call, `${Chat.prefix}/leaveChat`, this._id, member);
 	}
 
   /**
    * createMessage - add text to chat as a Message
    *
    * @param {string} text content of message
-   * @param {function} callback function to call when meteor method returns
+   * @returns {Promise} - a promise, 1 if successful
    */
-	createMessage(text, callback = () => {}) {
+	async createMessage(text) {
 	  check(text, String);
-	  check(callback, Function);
-	  Meteor.call(`${Chat.prefix}/createMessage`, this._id, text, callback);
+		return promiseCall(Meteor.call, `${Chat.prefix}/createMessage`, this._id, text);
 	}
 
   /**
@@ -78,14 +91,24 @@ class Chat {
    * @param {string} type of chat, e.g. private, group, public, etc
    * @param {string} title title of chat
    * @param {array} members users/players in chat
-   * @param {function} callback function to call when meteor method returns
+   * @returns {Promise} - a promise, 1 if successful
    */
-	static createChat(type = 'private', title = '', members = [], callback = () => {}) {
+	static async createChat(type = 'private', title = '', members = []) {
 	  check(type, String);
 	  check(title, String);
 	  check(members, [String]);
-	  check(callback, Function);
-	  Meteor.call(`${Chat.prefix}/createChat`, type, title, members, callback);
+	  return promiseCall(Meteor.call, `${Chat.prefix}/createChat`, type, title, members);
+	}
+
+	/**
+	 * message - a getter to get the array of messages associated with chat
+	 *
+	 * @readonly
+	 *
+	 * @memberOf Chat
+	 */
+	get messages() {
+		return Message.collection.find({ chatId: this._id }).fetch();
 	}
 }
 Chat.prefix = `freelancecourtyard:chat`;
--- packages/chatmessages/message-client.js
+++ packages/chatmessages/message-client.js
@@ -1,7 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check, /*Match*/ } from 'meteor/check';
+import { check, /* Match */ } from 'meteor/check';
 
+/**
+ * promiseCall - function to wrap async Meteor functions into returning promises
+ *
+ * @param  {Function} fn - async function to wrap
+ * @param  {Array} params - array of params
+ * @returns {Promise}           resolve if success
+ */
+function promiseCall(fn, ...params) {
+	return new Promise((resolve, reject) => {
+		fn(...params, (err, res) => {
+			if (err) { return reject(err); }
+			return resolve(res);
+		});
+	});
+}
 
 /**
  * class representing a message instance
@@ -9,110 +24,106 @@ import { check, /*Match*/ } from 'meteor/check';
  */
 class Message {
 
-  /**
-   * constructor - instantiate Message instance
-   * used to transform Message.collection items
-   * also see schema
-   *
-   * @param  {object} item object stored in collection
-   */
-  constructor(item) {
-    Object.assign(this, item);
-  }
+	/**
+	 * constructor - instantiate Message instance
+	 * used to transform Message.collection items
+	 * also see schema
+	 *
+	 * @param  {object} item object stored in collection
+	 */
+	constructor(item) {
+		Object.assign(this, item);
+	}
 
-  /**
-   * deleteMessage - delete this Message instance
-   *
-   * @param {function} callback function to call when meteor method returns
-   */
-  deleteMessage(callback = () => {}) {
-    check(callback, Function);
-    Meteor.call(`${Message.prefix}/deleteMessage`, this._id, callback);
-  }
+	/**
+	 * deleteMessage - delete this Message instance
+	 *
+	 * @return {Promise} - resolve to 1 if update success
+	 */
+	async deleteMessage() {
+		return promiseCall(Meteor.call, `${Message.prefix}/deleteMessage`, this._id);
+	}
 
-  /**
-   * editMessage - edit text content of this Message instance
-   *
-   * @param {string} text new content of Message
-   * @param {function} callback function to call when meteor method returns
-   */
-  editMessage(text, callback = () => {}) {
-    check(text, String);
-    check(callback, Function);
-    Meteor.call(`${Message.prefix}/editMessage`, this._id, text, callback);
-  }
+	/**
+	 * editMessage - edit text content of this Message instance
+	 *
+	 * @param {string} text new content of Message
+	 * @returns {Promise} - resolve to 1 if update success
+	 */
+	async editMessage(text, ) {
+		check(text, String);
+		return promiseCall(Meteor.call, `${Message.prefix}/editMessage`, this._id, text);
+	}
 
-  /**
-   * replyMessage - create a Message instance as reply to another Message instance
-   *
-   * @param {string} text content of reply Message
-   * @param {function} callback function to call when meteor method returns
-   */
-  replyMessage(text, callback = () => {}) {
-    check(text, String);
-    check(callback, Function);
-    Meteor.call(`${Message.prefix}/replyMessage`, this._id, text, callback);
-  }
+	/**
+	 * replyMessage - create a Message instance as reply to another Message instance
+	 *
+	 * @param {string} text content of reply Message
+	 * @returns {Promise} - resolve to 1 if update success
+	 */
+	async replyMessage(text, ) {
+		check(text, String);
+		return promiseCall(Meteor.call, `${Message.prefix}/replyMessage`, this._id, text);
+	}
 
-  /**
-   * createMessage - add an instance of Message to collection
-   * constructor is used to transform the collection, whereas this is to add a Message to collection
-   *
-   * @param {string} chatId id of chat Message belongs to
-   * @param {string} text content of Message
-   * @param {Message} replyTo Message object that is being replied to
-   * @param {function} callback function to call when meteor method returns
-   */
-  static createMessage(chatId, text, replyTo, callback = () => {}) {
-    check(chatId, String);
-    check(text, String);
-    check(replyTo, Object);
-    check(callback, Function);
-    Meteor.call(`${Message.prefix}/createMessage`, chatId, text, replyTo, callback);
-  }
+	/**
+	 * createMessage - add an instance of Message to collection
+	 * constructor is used to transform the collection, whereas this is to add a Message to collection
+	 *
+	 * @param {string} chatId id of chat Message belongs to
+	 * @param {string} text content of Message
+	 * @param {Message} replyTo Message object that is being replied to
+	 * @returns {Promise} - resolve to 1 if update success
+	 */
+	static async createMessage(chatId, text, replyTo, ) {
+		check(chatId, String);
+		check(text, String);
+		check(replyTo, Object);
+		return promiseCall(Meteor.call, `${Message.prefix}/createMessage`, chatId, text, replyTo);
+	}
 }
 Message.schema = {
-  _id: String,  // id of message
-  // roomId: String,
-  // gameId: String,
-  // teamId: String,
-  chatId: String,  // id of chat it belongs to
-  from: {
-    // user
-    _id: String,
-    displayName: String,  // this may be different from userId name
-  },
-  timestamp: Date,
-  text: String,  // NOTE: main thing
+	_id: String,  // id of message
+	// roomId: String,
+	// gameId: String,
+	// teamId: String,
+	chatId: String,  // id of chat it belongs to
+	from: {
+		// user
+		_id: String,
+		displayName: String,  // this may be different from userId name
+	},
+	timestamp: Date,
+	text: String,  // NOTE: main thing
 
-  // optional stuff
+	// optional stuff
 
-  // NOTE: rather than materializing it, might as well save a copy
-  replyTo: {
-    _id: String,
-    from: {},
-    text: String,
-    timestamp: Date,
-  },  // id of message replied to
-  // forwardFrom: String,  // id of forwarded message
-  editAt: Date,  // optional
-  // contain things like special parse instruction for system message
-  // system message can be member join, left, chat created
-  metadata: Object,
-  // audio: Object,
-  // document: Object,
-  // photo: Object,
-  // video: Object,
-  // caption: String,
-  // contact: Object,
-  // location: Object,
+	// NOTE: rather than materializing it, might as well save a copy
+	replyTo: {
+		_id: String,
+		from: {},
+		text: String,
+		timestamp: Date,
+	},  // id of message replied to
+	// forwardFrom: String,  // id of forwarded message
+	editAt: Date,  // optional
+	// contain things like special parse instruction for system message
+	// system message can be member join, left, chat created
+	metadata: Object,
+	// audio: Object,
+	// document: Object,
+	// photo: Object,
+	// video: Object,
+	// caption: String,
+	// contact: Object,
+	// location: Object,
 };
 Message.prefix = `freelancecourtyard:message`;
 
 Message.collection = new Mongo.Collection(`${Message.prefix}Collection`, {
-  transform: function(item) {
-    return new Message(item);
-  },
+	transform: function(item) {
+		return new Message(item);
+	},
 });
 
 export { Message };
