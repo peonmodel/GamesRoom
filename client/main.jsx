d9bbe6a9863f0900792843890b10c26a04a222e7
peonmodel <peonmodel+git@gmail.com>
2016-12-11 23:58:53 +0800

    cleaned up the login template code

--- client/main.jsx
+++ client/main.jsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 
 // import App from '../imports/ui/App.jsx';
 import { Component } from 'react';
-import { LoginLayout } from '../imports/ui/login.jsx';
+import { LoginContainer } from '../imports/ui/login.jsx';
 import { LobbyContainer } from '../imports/ui/lobby.jsx';
 
 Meteor.startup(() => {
@@ -15,7 +15,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<LoginLayout />
+				<LoginContainer />
 				<LobbyContainer />
 			</div>
 		);
--- imports/ui/login.jsx
+++ imports/ui/login.jsx
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import { Connection } from 'meteor/freelancecourtyard:connection';
 import React, { Component } from 'react';
-import { Container, Input, Button } from 'semantic-ui-react';
+import { Container, Button, Form, Header, Message, Label } from 'semantic-ui-react';
 import { _ } from 'lodash';
 // import ReactDOM from 'react-dom';
 import { reactify } from 'meteor/freelancecourtyard:reactivecomponent';
@@ -8,64 +9,120 @@ import { reactify } from 'meteor/freelancecourtyard:reactivecomponent';
 class GuestLogIn extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			formData: {},
+			placeholder: this.placeholder,
+			busy: false,
+			error: undefined,
+		};
 	}
+
+	get placeholder() {
+		const colorArray = [
+			'Pink', 'Blue', 'Orange', 'Green', 'Yellow', 'Fuchsia', 'Red', 'Golden', 'Silver', 'Brown'
+		];
+		const sizeArray = [
+			'Little', 'Small', 'Big', 'Fat', 'Tiny', 'Huge', 'Mini'
+		];
+		const emotionArray = [
+			'Happy', 'Sad', 'Depressed', 'Excited', 'Angry', 'Motivated', 'Scared', 'Energized', 'Surprised', 'Tired'
+		];
+		const nounArray = [
+			'Diamond', 'JetPack', 'Cruiser', 'Bunny', 'Lambini', 'Bird', 'Elephant', 'Frog', 'Player', 'Lover', 'Guest',
+			'User', 'Cow', 'Mustang', 'Student', 'Driver', 'Hawker', 'Villager', 'Explorer', 'Dancer', 'Stranger', 'Farmer', 'Boat', 'Car', 'Chicken', 'Machine', 'Thing'
+		];
+		const number = _.sample(_.range(10, 2110));
+		const tempName = _.sample(emotionArray) + _.sample(sizeArray) + _.sample(colorArray) + _.sample(nounArray) + number;
+		return tempName;
+	}
+
+	async handleSubmit(event, { formData }) {
+		event.preventDefault();
+		this.setState({ busy: true });
+		try {
+			await Connection.createGuest(formData.username || this.state.placeholder);
+		} catch (e) {
+			console.error(e);
+			this.setState({ error: true });
+		} finally {
+			this.setState({ busy: false });
+		}
+	}
+
+	handleChange() {
+		this.setState({ error: false });
+	}
+
 	render() {
 		return (
-			<div>
-				<Button>
-					Guest login
-				</Button>
-			</div>
+			<Form error={this.state.error} onSubmit={this.handleSubmit.bind(this)} onChange={this.handleChange.bind(this)}>
+				<Header as='h4'>Log in as guest</Header>
+				<Form.Field>
+					<label>Username</label>
+					<input name="username" placeholder={this.state.placeholder} />
+				</Form.Field>
+					<Message error header='Action Forbidden' content='username is used' />
+				<Button type='submit' loading={this.state.busy}>Guest Login</Button>
+			</Form>
 		);
 	}
 }
 
-// const GuestLoginForm = () => (
-// 	<Form>
-// 		<Header as='h4'>Log in as guest</Header>
-// 		<Button type='submit'>Guest Login</Button>
-// 	</Form>
-// );
-
-// const LogInForm = (props) => (
-// 	<Form>
-// 		<Header as='h4'>Log In</Header>
-// 		<Form.Field>
-// 			<label>Username</label>
-// 			<input placeholder={props.placeholder}/>
-// 		</Form.Field>
-// 		<Form.Field>
-// 			<label>Password</label>
-// 			<input type='password'/>
-// 		</Form.Field>
-// 		<Button type='submit' onClick={props.login}>Login</Button>
-// 	</Form>
-// );
-
-class RegisterOrLogin extends Component {
+class Logout extends Component {
 	constructor(props) {
-		Object.assign({
-			guestLogin() {}
-		}, props);
 		super(props);
 	}
+
+	async handleClick() {
+		try {
+			await Connection.logoutUser();
+			console.log(`logged out`);
+		} catch (error) {
+			console.error(`error logging out`);
+		}
+	}
+
 	render() {
+		const username = _.get(this.props.user, 'username') || '';
+		const isGuest = !_.get(this.props.user, 'profile.isRegistered');
 		return (
 			<Container>
-				<Input className="username" placeholder={this.props.tempName} />
-				<Input className="password" type="password" />
-				<Button>Login</Button>
-				<Button>Register</Button>
-				<GuestLogIn />
+				<Label>
+					{username}
+					<Label.Detail>{isGuest ? 'Guest' : 'User'}</Label.Detail>
+				</Label>
+				<Button onClick={this.handleClick.bind(this)}>Log out</Button>
 			</Container>
 		);
 	}
 }
 
-export class LoginLayout extends Component {
+export class Login extends Component {
+	constructor(props) {
+		super(props);  // this.props is "reactive", the other this attributes are reused
+	}
+
 	render() {
+		const loggedIn = !!this.props.user;
+		const isRegistered = !!_.get(this.props.user, 'profile.isRegistered');
+		let selectiveLogin = null;
+		if (!loggedIn) {
+			selectiveLogin = (<GuestLogIn />);
+		} else if (!isRegistered) {
+			selectiveLogin = (<Logout user={this.props.user}/>);
+		} else {
+			selectiveLogin = (<Logout user={this.props.user}/>);
+		}
 		return (
-			<RegisterOrLogin/>
+			<Container>
+				{ selectiveLogin }
+			</Container>
 		);
 	}
 }
+
+function reactiveMapper(props, onData) {
+	onData(null, { user: Meteor.user() });
+}
+
+export const LoginContainer = reactify(reactiveMapper)(Login);
