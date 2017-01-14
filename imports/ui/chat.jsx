471dc5643e2e3e4a887c027c16dabf3a9ef522a9
peonmodel <peonmodel+git@gmail.com>
2017-01-14 17:16:06 +0800

    minor edit to chat ui

--- imports/ui/chat.jsx
+++ imports/ui/chat.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Container, Input, Button, Card, Form, Comment, Header } from 'semantic-ui-react';
+import { Container, Input, Button, Card, Form, Comment, Header, Divider } from 'semantic-ui-react';
 import { _ } from 'lodash';
 // import ReactDOM from 'react-dom';
 import { reactify } from 'meteor/freelancecourtyard:reactivecomponent';
@@ -12,20 +12,34 @@ class MessageItem extends Component {
 		super(props);
 	}
 
+	// async replyMessage() {
+	// 	// return this.props.message.replyMessage()
+	// }
+
 	render() {
 		const message = this.props.message;
+		// let replyItem = '';
+		// if (!this.props.isReply) {
+		// 	const reply = this.props.messages.find(msg => msg._id === (message.replyTo || {})._id);
+		// 	if (reply) {
+		// 		replyItem = (
+		// 			<MessageItem message={reply} messages={this.props.messages} isReply={true}/>
+		// 		);
+		// 	}
+		// }
 		return (
 			<Comment>
 				<Comment.Avatar />
 				<Comment.Content>
+					{ /* replyItem */ }
 					<Comment.Author as='a'>{message.from.displayName}</Comment.Author>
 					<Comment.Text>{message.text}</Comment.Text>
 					<Comment.Metadata>
 						<div>{moment(message.timestamp).fromNow()}</div>
 					</Comment.Metadata>
-					<Comment.Actions>
+					{/* <Comment.Actions>
 						<Comment.Action>Reply</Comment.Action>
-					</Comment.Actions>
+					</Comment.Actions> */}
 				</Comment.Content>
 			</Comment>
 		);
@@ -68,15 +82,16 @@ export class Conversation extends Component {
 		if (!this.props.ready) { return (<div></div>); }
 		const chat = this.props.chat;
 		const messageArray = chat.messages.map(msg => {
-			return (<MessageItem message={msg} key={msg._id}/>);
+			return (<MessageItem message={msg} key={msg._id} messages={chat.messages} />);
 		});
 		return (
 			<Container className="currentChat">
 				<h1>{chat.title}</h1>
 				// add messages here
-				<Comment.Group>
+				<Comment.Group minimal>
 				<Header>Messages</Header>
 				{ messageArray }
+				<Divider />
 				<Form reply onSubmit={this.handleSubmit.bind(this)} onChange={this.handleChange.bind(this)}>
 					<Form.TextArea name="chatText"/>
 					<Button content='Add Reply' labelPosition='left' icon='edit' primary />
