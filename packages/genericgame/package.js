fba7831adb21b37d83ba7357ea16f662256549e6
peonmodel <peonmodel+git@gmail.com>
2017-01-20 00:37:36 +0800

    shifted test/ folders

--- packages/genericgame/package.js
+++ packages/genericgame/package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'freelancecourtyard:genericgame',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'freelancecourtyard:genericgame',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: '',
+	// URL to the Git repository containing the source code for this package.
+	git: '',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.4.0.1');
-  api.use('ecmascript');
-  api.mainModule('genericgame.js');
+Package.onUse(function setupPkg(api) {
+	api.versionsFrom('1.4.0.1');
+	api.use('ecmascript');
+	api.mainModule('genericgame.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('freelancecourtyard:genericgame');
-  api.mainModule('genericgame-tests.js');
+Package.onTest(function setupPkg(api) {
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('freelancecourtyard:genericgame');
+	api.mainModule('genericgame-tests.js');
 });
