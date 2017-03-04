2e083352ff77b39b8e353f3d0e508baf19eb3352
peonmodel <peonmodel+git@gmail.com>
2017-03-04 13:33:13 +0800

    exported genericGameInfoSchema

--- packages/genericgame/genericgame-client.js
+++ packages/genericgame/genericgame-client.js
@@ -1,3 +1,3 @@
 export { GenericGame, Player } from './client/genericgame.js';
 export { GenericGameInfo } from './client/genericgameinfo.js';
-export { genericGameSchema } from './imports/schema.js';
+export { genericGameSchema, genericGameInfoSchema } from './imports/schema.js';
